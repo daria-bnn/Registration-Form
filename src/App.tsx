a929a20b9246c589867da4dd7a8904e98598802c
Daria <daria.bnn@bk.ru>
2022-07-22 16:18:29 +0300

    create-request-and-add-types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './App.css'
+import RandomPicture from './components/RandomPicture/RandomPicture'
 
-const App = () => {
-  return <div className="App" />
-}
+const App = () => (
+  <div className="App">
+    <RandomPicture />
+  </div>
+)
 
 export default App
---
+++ src/api/getImage.ts
@@ -0,0 +1,14 @@
+import Image from '../types/types'
+
+const USER_KEY = ''
+const URL = `https://api.unsplash.com/photos/random?client_id=${USER_KEY}`
+
+const getImage = (): Promise<Image> =>
+  fetch(URL).then((res) => {
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error(`Ошибка ${res.status}`)
+  })
+
+export default getImage
