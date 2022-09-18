e71cc0edf079c60b4590ed2369f388e4fe125a2d
Daria <daria.bnn@bk.ru>
2022-09-18 22:04:46 +0300

    edited App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import React from 'react'
-
-// import Form from './components/Form/Form'
-// import ImageContainer from './components/ImageContainer/ImageContainer'
-// import Select from './components/SelectExample/Select'
-
-// import Menu from './components/Menu/Menu'
 import cnApp from './App.classname'
 import FilterBlock from './components/search-and-filter/FilterBlock/FilterBlock'
 import PixelForm from './components/PixelForm/PixelForm'
-
-import './App.css'
 import ScalableBlock from './components/ScalableBlock/ScalableBlock'
-// import RandomImage from './components/random-image/RandomImage/RandomImage'
+import RandomImage from './components/random-image/RandomImage/RandomImage'
 import Menu from './components/drop-down-menu/Menu/Menu'
 import FormMaterial from './components/FormMaterial/FormMaterial'
-// const OPTIONS = [
-//   { value: 'Angular', text: 'Angular' },
-//   { value: 'React', text: 'React' },
-// ]
+
+import './App.css'
 
 const App = () => (
   <div className={cnApp()}>
-    {/* <RandomImage /> */}
     <ScalableBlock classname={cnApp('ScalableBlock')} />
+    <RandomImage />
     <Menu />
     <FilterBlock />
     <PixelForm />
